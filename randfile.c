921ec031a0bf821fe3e1ad9c788be3d6f019ccf0
Tao Lin <lin.tao.5711y@gmail.com>
2016-10-26 15:35:28 -0400

    started

---
+++ randfile.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+
+
+
+
+
+int randInt(){
+  int fd = open("/dev/random",O_RDONLY);
+  int randLum ;
+  //handle errors
+  read(fd,&randLum,4);
+  close(fd);
+  return randLum;
+}
+
+void printArray(int *arr){
+  
+}
+
+int main(){
+  int *array = (int*)malloc(40);
+  
+  //filling array up
+  int ctr = 0;
+  while(ctr < 10){
+    array[ctr] = randInt();
+    ctr++;
+  }
+  
+  //writing array into file
+  umask(0);
+  int fd = open("randFile.txt", O_CREAT | O_APPEND, 0666);
+
+  while(*array){
+    write(fd,array,4);
+    array++;
+    printf("wtf");
+  }
+
+  close(fd);
+  
+  printArray(array);
+  return 0;
+}
