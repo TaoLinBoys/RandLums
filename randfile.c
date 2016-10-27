033aacdbbb03478a2eec807a20728dcdadb94d34
Tao Lin <lin.tao.5711y@gmail.com>
2016-10-26 23:36:49 -0400

    completed

--- randfile.c
+++ randfile.c
@@ -6,54 +6,51 @@
 #include <sys/stat.h>
 
 
-
-
-
 int randInt(){
   int fd = open("/dev/random",O_RDONLY);
-  int randLum ;
+  int randLum;
   //handle errors
   read(fd,&randLum,4);
   close(fd);
   return randLum;
 }
 
-void printArray(int *arr){
-  while (*arr){
-	printf("%d, ",*arr);
-	arr++;
-  }
-  
-}
 
 int main(){
-  int *array = (int*)malloc(40);
-  
-  //filling array up
-  int ctr = 0;
-  int *temp = array;
-  while(ctr<10){
-    *array = randInt();
-	array++;
-    ctr++;
+  int arr[10];
+
+  //filling array
+  int ctr;
+  printf("First set of random numbers:\n");
+  for(ctr = 0; ctr < 10; ctr++){
+    arr[ctr] = randInt();
+    printf("random %d: %d\n", ctr, arr[ctr]);
   }
+  printf("\n");
 
-
-  //writing array into file
+  
+  //writing to file
+  printf("Writing to file...\n");
   umask(0);
-  int fd = open("randFile.txt", O_CREAT | O_APPEND, 0666);
-
-  
-  while(*temp){
-	if (write(fd,temp,4) == -1){
-		printf("what");
-	}
-    temp++;
-  }
-
+  int fd = open("randFile.txt", O_CREAT | O_RDWR, 0666);
+  write(fd, arr, sizeof(arr)); 
   close(fd);
 
-  printArray(array);
-  free(array);
+  
+  //reading from file
+  printf("Reading from file...\n\n");
+  fd = open("randFile.txt", O_RDONLY);
+  int readArr[10];
+  read(fd, readArr, sizeof(readArr));
+  close(fd);
+
+
+  //printing again
+  printf("Second set of random numbers:\n");
+  for(ctr = 0; ctr < 10; ctr++){
+    printf("random %d: %d\n", ctr, readArr[ctr]);
+  }
+  
+  
   return 0;
 }
